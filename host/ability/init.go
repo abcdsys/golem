978188cf88758d1101f7361ad44f618e14963abc
X <merlin9710@gmail.com>
2026-05-19 17:16:14 +0800

    🎉 init(ability): 新增能力层初始化文件并组织子模块

---
+++ host/ability/init.go
@@ -0,0 +1,13 @@
+package ability
+
+import _ "github.com/sbgayhub/golem/host/ability/cdn"
+import _ "github.com/sbgayhub/golem/host/ability/contact"
+import _ "github.com/sbgayhub/golem/host/ability/favor"
+import _ "github.com/sbgayhub/golem/host/ability/group"
+import _ "github.com/sbgayhub/golem/host/ability/label"
+import _ "github.com/sbgayhub/golem/host/ability/message"
+
+// Initial 初始化能力层
+func Initial() error {
+	return nil
+}
